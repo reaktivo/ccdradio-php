64b5318ed796976cef0f19273f266716effbfbea
reaktivo <m@reaktivo.com>
2014-06-17 19:45:12 -0500

    better mobile layout

--- escucha.php
+++ escucha.php
@@ -1,15 +1,17 @@
 <?php include('_top.php') ?>
 <div class="escucha">
-  <div class="en-construccion"><p>Actualmente nos encontramos trabajando en nuestra
-página web y cabina de radio, este es un video en
-tiempo real de nuestro proceso.</p>
-<p>Te invitamos a escuchar
-nues&shy;tra pro&shy;gra&shy;ma&shy;ción.</p>
-
-  </div>
   <a href="#" class="escucha-btn">
     <i class="play fa fa-play"></i><span class="on-air"><span>Al&nbsp;aire</span></span><span class="text">Escucha</span>
     <div class="label"><span>Estás escuchando:</span><span class="now-playing">CCD Radio</span></div>
   </a>
+
+  <div class="en-construccion"><p>Actualmente nos encontramos trabajando en nuestra
+página web y cabina de radio, este es un video en
+tiempo real de nuestro&nbsp;proceso.</p>
+<p>Te invitamos a escuchar
+nuestra&nbsp;programación.</p>
+
+  </div>
+
 </div>
 <?php include('_bottom.php') ?>
